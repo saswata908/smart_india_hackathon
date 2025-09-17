a2eebb5ee54f52149f7539311bf10134e8412e82
saswata908 <157130478+saswata908@users.noreply.github.com>
2025-09-17 18:45:11 +0530

    Add files via upload

---
+++ lib/screens/quests.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Quests extends StatefulWidget {
+  @override
+  State<Quests> createState() => _QuestsState();
+}
+
+class _QuestsState extends State<Quests> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white);
+  }
+}
